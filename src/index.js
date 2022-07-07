9688b295581a4384501b5cbe3da92e39753277ef
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-07 01:10:45 +0000

    Display and storoge default tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
+import { trim } from 'lodash';
 import UI from '../modules/UI';
 import { markAsComplete, clearAllCompleteTask, clearForm } from '../modules/EventsHandles';
+import Storage from '../modules/Storage';
+import validate from '../modules/Validation';
 
 const tasks = [
   {
@@ -43,19 +46,34 @@ const tasks = [
 const addForm = document.querySelector('form');
 addForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  const tastsInStorage = Storage.getTasks();
   const task = {
-    description: addForm.elements.description.value,
+    description: trim(addForm.elements.description.value),
     completed: false,
+    index: tastsInStorage.length,
   };
-  clearForm();
-  UI.addTask(task);
-  UI.editList();
-  markAsComplete();
+  if (validate(task)) {
+    Storage.addTask(task);
+    clearForm();
+    UI.addTask(task);
+    UI.editList();
+    markAsComplete();
+  }
 });
 
-// display already added tasks when page loads
-UI.displayTasks(tasks);
+// display default task when task in storage is empty
+if (Storage.getTasks().length === 0) {
+  // add default tasks to storage
+  tasks.forEach((task) => {
+    Storage.addTask(task);
+  });
+  UI.displayTasks(tasks);
+} else {
+  const saveTasks = Storage.getTasks();
+  UI.displayTasks(saveTasks);
+}
+
 // execute the following functions when page loads
 markAsComplete();
-UI.editList();
 clearAllCompleteTask();
+UI.editList();
