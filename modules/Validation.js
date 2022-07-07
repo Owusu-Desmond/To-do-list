1ded0a42c4537a00f75e8dfc1c32da21c0fbf308
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-07 01:07:10 +0000

    Adjust with other changes

--- modules/Validation.js
+++ modules/Validation.js
@@ -1,19 +1,18 @@
-import UI from "./UI";
+import UI from './UI';
 
 const validate = (task) => {
-    const tasksDescriptions = document.querySelectorAll('.task-description');
-    let taskAdded = 'no';
-    tasksDescriptions.forEach(td => {
-        if (td.innerHTML === task.description) {
-            taskAdded = 'yes';
-            UI.showErrorMessage('Task already added.')
-        }
-    })
-    if (taskAdded === 'yes') {
-        return false;
-    }else{
-        return true
+  const tasksDescriptions = document.querySelectorAll('.task-description');
+  let taskAdded = 'no';
+  tasksDescriptions.forEach((td) => {
+    if (td.innerHTML === task.description) {
+      taskAdded = 'yes';
+      UI.showErrorMessage('Task already added.');
     }
-}
+  });
+  if (taskAdded === 'yes') {
+    return false;
+  }
+  return true;
+};
 
 export default validate;
