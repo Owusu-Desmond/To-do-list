43696946874b399f34330933760c178a31da9021
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-08 10:38:45 +0000

    Fix

--- modules/EventsHandles.js
+++ modules/EventsHandles.js
@@ -1,6 +1,6 @@
+import { trim } from 'lodash';
 import Storage from './Storage';
 import UI from './UI';
-import { trim } from 'lodash';
 // mark task as completed
 const markAsComplete = () => {
   const checkBoxes = document.querySelectorAll('.form-check-input');
@@ -33,51 +33,53 @@ const clearForm = () => {
 
 const saveEdited = (deleteBtn, threeDotBtn, taskDescription, list) => {
   // remove delete btn
-  deleteBtn.classList.add('d-none')
-  threeDotBtn.classList.remove('d-none')
+  deleteBtn.classList.add('d-none');
+  threeDotBtn.classList.remove('d-none');
   // remove contentEditable attribute
   taskDescription.removeAttribute('contentEditable');
   // remove list background color
-  list.classList.remove('bg-light')
-}
+  list.classList.remove('bg-light');
+};
 
 const editList = () => {
   const allThreeDotBtn = document.querySelectorAll('.bi-three-dots-vertical');
   allThreeDotBtn.forEach((threeDotBtn) => {
-    threeDotBtn.addEventListener( 'click', () => {
+    threeDotBtn.addEventListener('click', () => {
       const list = threeDotBtn.parentElement;
       const taskDescription = list.firstElementChild.lastElementChild;
       // add backgroud color to list
-      list.classList.add('bg-light')
+      list.classList.add('bg-light');
       // set content to editable autofocus and check spellings
       taskDescription.setAttribute('contentEditable', true);
       taskDescription.setAttribute('spellCheck', true);
       taskDescription.focus();
-      threeDotBtn.classList.add('d-none')
+      threeDotBtn.classList.add('d-none');
       // delete task when neccessary
       const deleteBtn = threeDotBtn.previousElementSibling;
-      deleteBtn.classList.remove('d-none')
+      deleteBtn.classList.remove('d-none');
       UI.deleteTask();
       // Update to storage after user input
       let task = taskDescription.innerText;
       taskDescription.addEventListener('input', () => {
         const newTask = taskDescription.innerText;
-        Storage.updateTask(task, newTask)
+        Storage.updateTask(task, newTask);
         task = newTask;
-      })
+      });
       // Update to UI if enter key is pressed
       taskDescription.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
           event.preventDefault();
           // check if the edited list is not empty
           if (trim(taskDescription.innerText).length < 4) {
-            UI.showErrorMessage('Edited task length is too short')
+            UI.showErrorMessage('Edited task length is too short');
             return;
           }
-          saveEdited(deleteBtn,threeDotBtn, taskDescription, list);
+          saveEdited(deleteBtn, threeDotBtn, taskDescription, list);
         }
       });
     });
   });
-}
-export { clearAllCompleteTask, markAsComplete, clearForm, editList };
+};
+export {
+  clearAllCompleteTask, markAsComplete, clearForm, editList,
+};
