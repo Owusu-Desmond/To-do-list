b3aaf9372e053abe6c727974c4e56d35865ca74e
Owusu-Desmond <desmond0406114009@gmail.com>
2022-07-13 16:10:35 +0000

    Add tasks mock

---
+++ __mocks__/tasks.js
@@ -0,0 +1,14 @@
+const tasks = [
+    {
+        description : "Have doctor appointment",
+        conpleted : true,
+        index : 0,
+    },
+    {
+        description : "Take lessons at microverse ",
+        conpleted : true,
+        index : 1,
+    },
+]
+
+export default tasks;
